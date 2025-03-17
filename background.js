952c9f0569c08a5bde29ac462d7be75ebf08edbe
Gwouigwoui <guiguitare@yahoo.fr>
2025-03-17 14:03:23 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,13 @@
-importScripts('js/storage.js');
+// Détection de l'environnement (Firefox ou Chrome)
+const runtime = typeof browser !== 'undefined' ? browser.runtime : chrome.runtime;
+const tabs = typeof browser !== 'undefined' ? browser.tabs : chrome.tabs;
 
+// Importation de storage.js en tenant compte de l'environnement
+if (typeof browser !== 'undefined') {
+  importScripts('js/storage.js');
+} else {
+  importScripts('js/storage.js');
+}
 
 function encodeURIComponentSafe(value) {
     return encodeURIComponent(value)
@@ -10,15 +18,28 @@ function encodeURIComponentSafe(value) {
         .replace(/\)/g, '%29');
 }
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+// Fonction pour exécuter les requêtes de manière compatible
+function executeRequest(url, options = {}, callback) {
+  fetch(url, options)
+    .then(response => response.json())
+    .then(json => callback(json))
+    .catch(error => {
+      console.error('Fetch error:', error);
+      callback({ error: 'Unable to connect' });
+    });
+}
+
+// Utilisation de l'API compatible pour les deux navigateurs
+runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.contentScriptQuery === 'queryMedia') {
         console.log(`Querying ${request.mediaType} '${request.tmdbId}'`);
         pullStoredData(function() {
             const options = {headers: {'X-Api-Key': serverAPIKey}};
-            fetch(`${origin}/api/v1/${request.mediaType}/${encodeURIComponent(request.tmdbId)}`, options)
-                .then(response => response.json())
-                .then(json => sendResponse(json))
-                .catch(error => console.error(error))
+            executeRequest(
+              `${origin}/api/v1/${request.mediaType}/${encodeURIComponent(request.tmdbId)}`,
+              options,
+              sendResponse
+            );
         });
         return true;
     }
@@ -31,10 +52,11 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
                 headers: {'Content-Type': 'application/json', 'X-Api-Key': serverAPIKey},
                 body: JSON.stringify({mediaType: request.mediaType, mediaId: request.tmdbId, seasons: request.seasons})
             };
-            fetch(`${origin}/api/v1/request`, options)
-                .then(response => response.json())
-                .then(json => sendResponse(json))
-                .catch(error => console.error(error));
+            executeRequest(
+              `${origin}/api/v1/request`,
+              options,
+              sendResponse
+            );
         });
         return true;
     }
@@ -43,10 +65,11 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         console.log(`Searching movie '${request.title}'`);
         pullStoredData(function() {
             const options = {headers: {'X-Api-Key': serverAPIKey}};
-            fetch(`${origin}/api/v1/search?query=${encodeURIComponentSafe(request.title)}`, options)
-                .then(response => response.json())
-                .then(json => sendResponse(json))
-                .catch(error => console.error(error));
+            executeRequest(
+              `${origin}/api/v1/search?query=${encodeURIComponentSafe(request.title)}`,
+              options,
+              sendResponse
+            );
         });
         return true;
     }
@@ -56,10 +79,11 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         let plexToken = encodeURIComponentSafe(request.plexToken);
         console.log(`Requesting Plex media '${mediaKey}'`);
         const options = {headers: {'Accept': 'application/json'}};
-        fetch(`https://metadata.provider.plex.tv/library/metadata/${mediaKey}?X-Plex-Token=${plexToken}`, options)
-            .then(response => response.json())
-            .then(json => sendResponse(json))
-            .catch(error => console.error(error));
+        executeRequest(
+          `https://metadata.provider.plex.tv/library/metadata/${mediaKey}?X-Plex-Token=${plexToken}`,
+          options,
+          sendResponse
+        );
         return true;
     }
 
@@ -67,10 +91,11 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         console.log(`Getting Overseerr version`);
         pullStoredData(function() {
             const options = {headers: {'X-Api-Key': serverAPIKey}};
-            fetch(`${origin}/api/v1/status`, options)
-                .then(response => response.json())
-                .then(json => sendResponse(json))
-                .catch(error => console.error(error));
+            executeRequest(
+              `${origin}/api/v1/status`,
+              options,
+              sendResponse
+            );
         });
         return true;
     }
@@ -82,29 +107,52 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             fetch(`${origin}/api/v1/auth/me`, options)
                 .then(response => response.json())
                 .then(json => sendResponse(Object.keys(json).filter((key) => /jellyfin/.test(key)).length > 0))
-                .catch(error => console.error(error));
+                .catch(error => {
+                    console.error(error);
+                    sendResponse(false);
+                });
         });
         return true;
     }
 
     else if (request.contentScriptQuery === 'openOptionsPage') {
-        chrome.runtime.openOptionsPage();
+        runtime.openOptionsPage();
         return true;
     }
 
     else if (request.contentScriptQuery === 'listenForUrlChange') {
-        chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+        tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
             if (
                 changeInfo.status === 'complete' &&
                 tab.status === 'complete' &&
                 tab.url &&
                 tab.url.startsWith('https://www.senscritique.com')
             ) {
-                chrome.tabs.sendMessage(tab.id, {
+                tabs.sendMessage(tab.id, {
                     newUrl: tab.url
                 });
             }
         });
     }
+    
+    // Ajouter un gestionnaire spécifique pour l'authentification
+    else if (request.contentScriptQuery === 'authenticate') {
+        console.log('Authenticating to Overseerr');
+        const options = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                email: request.username,
+                password: request.password
+            })
+        };
+        executeRequest(
+          `${request.url}/api/v1/auth/login`,
+          options,
+          sendResponse
+        );
+        return true;
+    }
+    
     return false;
 });
